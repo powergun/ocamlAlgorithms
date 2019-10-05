89771bbd24afdc5b675ef0174613791a848799c5
Wei Ning <macgnw@gmail.com>
2019-10-05 13:20:34 +1000

    slice

--- scripts/containers/lists/slice_99_18.ml
+++ scripts/containers/lists/slice_99_18.ml
@@ -1,7 +1,22 @@
 #!/usr/bin/env ocaml
 
+let slice_incl n m xs = 
+  let rec slice' index acc = function
+    | [] -> acc
+    | x :: xs -> if index >= n && index <= m then 
+                   slice' (index + 1) (x :: acc) xs
+                 else
+                   slice' (index + 1) acc xs
+  in
+    List.rev (slice' 0 [] xs)
+  ;;
+
 let runDemos () = 
-  1;
+  let sut = ["a";"b";"c";"d";"e";"f";"g";"h";"i";"j"] in
+  assert(slice_incl 2 6 sut = ["c"; "d"; "e"; "f"; "g"]);
+  assert(slice_incl (-1) 999 sut = sut);
+  assert(slice_incl 6 2 sut = []);
+  assert(slice_incl 2 6 [] = []);
   ;;
 
 runDemos();;
