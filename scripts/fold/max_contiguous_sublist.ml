5a201a912f5397c3c393b9d756bd230356bfee33
Wei Ning <macgnw@gmail.com>
2019-10-09 22:15:30 +1100

    debug technique

--- scripts/fold/max_contiguous_sublist.ml
+++ scripts/fold/max_contiguous_sublist.ml
@@ -6,10 +6,23 @@ source:
 https://www.youtube.com/watch?v=KiUjTObV3Io&t=45s
 *)
 
+(* 
+document a debugging technique, learned from: https://www.youtube.com/watch?v=DGvJk14sfi8
+in the follow code I could have written
+if max_current < 0 then 0
+
+but instead I want to tell how many times this spot is executed:
+if max_current < 0 then (let _ = print_endline "called" and i = 0 in i)
+- use group imperial notation (begin end could do too)
+- there are other ways to acheive the print, but I followed the video and used a dummy
+ variable; print_endline returns unit which is bound to the dummy
+- make sure the group ends with the original (return) value
+
+*)
 let max_consub l =
   let f (max_so_far, max_current) elem =
     let max_so_far' = max max_so_far (max_current + elem) in
-    let max_current' = if max_current < 0 then 0 else max_current in
+    let max_current' = if max_current < 0 then (let _ = print_endline "called" and i = 0 in i) else max_current in
     (max_so_far', max_current' + elem) in
 
   let (max_so_far, current_max) = List.fold_left f (0, 0) l in
