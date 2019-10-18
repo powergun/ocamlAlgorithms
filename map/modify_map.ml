b6638976bdfb3620072114a245ac16d03157dc07
Wei Ning <wei@canva.com>
2019-10-18 14:07:11 +1100

    renamed map to map_and_hash

--- map/modify_map.ml
+++
@@ -1,5 +0,0 @@
-
-let () =
-  Stdio.printf "....\n"
-;;
-
--- map_and_hash/association_list.ml
+++ map_and_hash/association_list.ml
@@ -5,7 +5,6 @@ open Core
 http://dev.realworldocaml.org/maps-and-hashtables.html
 *)
 
-
 let%test "create association list" =
   let digit_alist =
     [ 0, "zero"; 1, "one"; 2, "two"  ; 3, "three"; 4, "four"
@@ -20,6 +19,14 @@ let%test "create association list" =
   let v = List.Assoc.find ~equal:Int.equal al 3242 in
   assert(v = Some "idd");
 
+  (* 
+  Association lists are simple and easy to use, but their performance 
+  is not ideal, since almost every nontrivial operation on an association 
+  list requires a linear-time scan of the list.
+  *)
   true
 ;;
+
+
+
 
